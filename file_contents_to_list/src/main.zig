eaef21dc59faa6796af25077b9517c42fe497eb7
JJvanWyk <49305075+JJvanWyk@users.noreply.github.com>
2021-04-03 00:27:37 +1100

    Add files via upload

---
+++ file_contents_to_list/src/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const testing = std.testing;
+
+pub fn file_contents_to_list (
+        allocator: *std.mem.Allocator,
+        relative_file_path: []const u8,
+    ) !std.ArrayList(u8) {
+    const file = try std.fs.cwd().openFile(relative_file_path, .{});
+    defer file.close();
+    const file_size = (try file.stat()).size;
+    const file_buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(file_buffer);
+    _ = try file.readAll(file_buffer);
+
+    var list_of_strings = std.ArrayList(u8).init(allocator);
+
+    for (file_buffer) |character| {
+        try list_of_strings.append(character);
+    }
+    return list_of_strings;
+}
